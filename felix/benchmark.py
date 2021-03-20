41e63061d37f11ee5e3b2fb357dab585caadc907
javis-code <phoenixfre.yt@gmail.com>
2021-03-20 18:52:21 +0100

    benchmark II

--- felix/benchmark.py
+++ felix/benchmark.py
@@ -6,7 +6,7 @@ i = 0
 
 print("Starting addition benchmark...")
 
-while i < 25000:
+while i < 10000:
     print("Iteration {}".format(i))
     i += 1
 
@@ -14,13 +14,33 @@ add_bm_time_one = round(time.time())
 
 i = 0
 
-while i < 25000:
+while i < 10000:
     print("Iteration {}".format(i))
     i += 1
 
 add_bm_time_two = round(time.time())
 
+i = 0
+
+# TB => Time Benchmark // IT => Iteration
+
+while i < 10000:
+    print("TB {} -- IT {}".format(time.time(), i))
+    i += 1
+
+add_bm_time_three = round(time.time())
+
+i = 0
+
+while i < 10000:
+    print("TB {} -- IT {}".format(time.time(), i))
+    i += 1
+
+add_bm_time_four = round(time.time())
+
 score = round(((add_bm_time_one - start_time) +
-               (add_bm_time_two - add_bm_time_one)) / 2)
+               (add_bm_time_two - add_bm_time_one) +
+               (add_bm_time_three - add_bm_time_two) +
+               (add_bm_time_four - add_bm_time_three)) / 4)
 
 print(score)
