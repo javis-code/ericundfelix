1707d3b365dff9354bbba416c1288af949d4de58
javis-code <phoenixfre.yt@gmail.com>
2021-03-20 18:57:03 +0100

    bm 3

--- felix/benchmark.py
+++ felix/benchmark.py
@@ -1,6 +1,6 @@
 import time
 
-start_time = round(time.time())
+start_time = time.time()
 
 i = 0
 
@@ -10,7 +10,7 @@ while i < 10000:
     print("Iteration {}".format(i))
     i += 1
 
-add_bm_time_one = round(time.time())
+add_bm_time_one = time.time()
 
 i = 0
 
@@ -18,7 +18,7 @@ while i < 10000:
     print("Iteration {}".format(i))
     i += 1
 
-add_bm_time_two = round(time.time())
+add_bm_time_two = time.time()
 
 i = 0
 
@@ -28,7 +28,7 @@ while i < 10000:
     print("TB {} -- IT {}".format(time.time(), i))
     i += 1
 
-add_bm_time_three = round(time.time())
+add_bm_time_three = time.time()
 
 i = 0
 
@@ -36,11 +36,11 @@ while i < 10000:
     print("TB {} -- IT {}".format(time.time(), i))
     i += 1
 
-add_bm_time_four = round(time.time())
+add_bm_time_four = time.time()
 
-score = round(((add_bm_time_one - start_time) +
-               (add_bm_time_two - add_bm_time_one) +
-               (add_bm_time_three - add_bm_time_two) +
-               (add_bm_time_four - add_bm_time_three)) / 4)
+score = ((add_bm_time_one - start_time) +
+        (add_bm_time_two - add_bm_time_one) +
+        (add_bm_time_three - add_bm_time_two) +
+         (add_bm_time_four - add_bm_time_three) / 4)
 
 print("Final Score: {}".format(score))
