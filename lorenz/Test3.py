186dd3a1788f62d5d4e82ecaea92be76cb1679d6
Lorenzrmr <reimerlorenz2004@gmail.com>
2021-03-20 20:48:10 +0100

    Quiz

--- lorenz/Test3.py
+++ lorenz/Test3.py
@@ -1,3 +1,7 @@
+print("Welche Farbe haben Bananen?")
+List_1 = ["Blau","Gelb","Grün","Rot"]
+for item in List_1:
+    print(item)
 Antwort = input("Ihre Antwort hier: ")
 
 blau = ["blau"]
@@ -8,10 +12,10 @@ grün = ["Grün"]
 if Antwort.lower().strip() in blau:
     print("Nein, Blau ist falsch! Niemand mag dich")
 elif Antwort.lower().strip() in gelb:
-    print("Gelb ist Richtig....-Geheime Bodschaft-")
+    print("Gelb ist Richtig")
 elif Antwort.lower().strip() in grün:
     print("Grün ist falsch du Fisch")
 elif Antwort.lower().strip() in rot:
     print("Rot ist falsch du Schwanznase")
 else: 
-    print("Falsch")
+    print("DU BIST DER HÄSSLICHSTE MENSCH DEN ICH JE GESEHEN HABE!!!!!!!!")
